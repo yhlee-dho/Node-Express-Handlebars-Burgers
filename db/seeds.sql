3a77cbb53bee0d878ecd39d88a89c837d1ad2e5b
unknown <yhlee89.dho@gmail.com>
2020-02-01 01:44:38 -0600

    handlebars are broken. need rework

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-USE burger_db;
+USE burgers_db;
 
 INSERT INTO burgers (burger_name, devoured) VALUES ('Double Quarter Poundher', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Animal Stale Double Double', true);
