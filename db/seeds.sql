0333cabac552ce2efd784c470c080ab41a980058
unknown <yhlee89.dho@gmail.com>
2020-01-31 19:58:16 -0600

    ok db is working now omg

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 USE burger_db;
 
-INSERT INTO burgers ('burger_name', devoured) VALUES ('Double Quarter Poundher', false);
-INSERT INTO burgers ('burger_name', devoured) VALUES ('Animal Stale Double Double', true);
-INSERT INTO burgers ('burger_name', devoured) VALUES ('Blue Cheeze with Eggg', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Quarter Poundher', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Animal Stale Double Double', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Blue Cheeze with Eggg', true);
