8878dc31a9a948c55f269f9bda443b1f66d78228
unknown <yhlee89.dho@gmail.com>
2020-02-01 16:42:18 -0600

    burgers.js for index

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,42 @@
+// on click
+$(function() {
+  $(".change-devour").on("click", function(event) {
+    event.preventDefault();
+    const id = $(this).data("id");
+    const newDevoured = {
+      devoured: 1
+    };
+    // send put
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevoured
+    })
+      // reload
+    .then(
+      function() {
+        location.reload();
+      }
+    );
+  });
+
+  // on submit
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+    var newBurger = {
+      burger_name: $("#newBurger").val().trim()
+    };
+
+    // send post
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    })
+      // reload
+    .then(
+      function() {
+        console.log("created new burger");
+        location.reload();
+      }
+    );
+  });
+});
