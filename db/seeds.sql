6b8b4e36d667d59ab3b97203b0a5d1076f177d84
unknown <yhlee89.dho@gmail.com>
2020-01-31 19:50:38 -0600

    updating seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,3 +2,4 @@ USE burger_db;
 
 INSERT INTO burgers ('burger_name', devoured) VALUES ('Double Quarter Poundher', false);
 INSERT INTO burgers ('burger_name', devoured) VALUES ('Animal Stale Double Double', true);
+INSERT INTO burgers ('burger_name', devoured) VALUES ('Blue Cheeze with Eggg', true);
